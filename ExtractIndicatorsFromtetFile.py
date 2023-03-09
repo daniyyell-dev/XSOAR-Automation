f350e179afb61b0801bd834a96deb7dffdd49395
Daniel Jeremiah <121419973+daniyyell-dev@users.noreply.github.com>
2023-03-09 09:18:50 +0100

    Create ExtractIndicatorsFromtetFile.py

---
+++ ExtractIndicatorsFromtetFile.py
@@ -0,0 +1,47 @@
+register_module_line('ExtractIndicatorsFromTextFile', 'start', __line__())
+
+
+
+
+
+def extract_indicators_from_file(args):
+    try:
+        maxFileSize = int(args.get('maxFileSize'))
+    except Exception:
+        maxFileSize = 1024 ** 2
+
+    res = demisto.executeCommand('getFilePath', {
+        'id': args.get('entryID')
+    })
+
+    try:
+        filePath = res[0]['Contents']['path']
+    except Exception:
+        raise FileNotFoundError
+
+    with open(filePath, mode='r') as f:
+        data = f.read(maxFileSize)
+
+        # Extract indicators (omitting context output, letting auto-extract work)
+        indicators_hr = demisto.executeCommand("extractIndicators", {
+            'text': data})[0][u'Contents']
+        return {
+            'Type': entryTypes['note'],
+            'ContentsFormat': formats['text'],
+            'Contents': indicators_hr,
+            'HumanReadable': indicators_hr
+        }
+
+
+def main():
+    try:
+        args = demisto.args()
+        demisto.results(extract_indicators_from_file(args))
+    except FileNotFoundError:
+        return_error("File was not found")
+
+
+if __name__ in ['__main__', 'builtin', 'builtins']:
+    main()
+
+register_module_line('ExtractIndicatorsFromTextFile', 'end', __line__())
